6c10e5a6624dad5ee3ce923570d4c4acdf403355
beijinaqie <m13273794916@163.com>
2021-11-29 18:17:09 +0800

    界面美化

--- src/server.js
+++ src/server.js
@@ -6,10 +6,10 @@ const { createReadStream } = require('fs')
 const fs = require('fs')
 const ejs = require('ejs')
 // const ora = require('ora')
-
+const mime = require('mime')
+const figlet = require('figlet')
 class Server {
   constructor(options) {
-    console.log(options);
     this.port = options.port
     this.showHideFile = options.showHideFile
     this.directory = path.resolve(process.cwd(), options.directory)
@@ -20,26 +20,37 @@ class Server {
     this.createServer()
   }
   sendFile(req, res, directory) {
-    return createReadStream(directory).pipe(res)
+    res.setHeader('Content-Type', mime.getType(directory) + ';charset=utf-8')
+    createReadStream(directory).pipe(res)
   }
   sendDir(req, res, directory) {
-    const content = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8')
+    const content = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8')
     let dirs = fs.readdirSync(directory)
+
     dirs = dirs.map(item => {
+      let statObj = fs.statSync(directory + '/' + item)
       return (!item.startsWith('.') || this.showHideFile) && {
         url: path.join(req.url, item),
-        filename: item
+        filename: item,
+        info: {
+          lastModified: (statObj.ctime).toLocaleString(),
+          isFile: statObj.isFile(), 
+          size: (statObj.size) / 1024 + 'k'
+        }
       }
-    })
+    }).filter(Boolean)
     let html = ejs.render(content, { dirs })
+    res.setHeader('Content-Type', 'text/html;charset=utf-8')
     res.end(html)
   }
   createServer() {
     const server = http.createServer((req, res) => {
       // ora.start()
-      if (req.url == '/favicon.ico') return
+      console.log(req.url);
       let directory = path.join(this.directory, req.url == '/' ? '' : req.url)
-      // console.log(directory);
+
+      if (req.url == '/favicon.ico') return
+
       if (!fs.existsSync(directory)) {
         res.end('Not Found')
       } else if (fs.statSync(directory).isFile()) {
@@ -54,9 +65,11 @@ class Server {
       }
     })
     server.listen(this.port, () => {
+      console.log(chalk.yellow.italic(figlet.textSync('welcome to test server')));
       console.log('your server is running at:\n');
       console.log('  ' + chalk.green('http://' + ip.address() + ':' + this.port));
       console.log('  ' + chalk.green('http://127.0.0.1:'+ this.port));
+      console.log('\n' + chalk.cyan.bold.italic('Hit CTRL-C to stop the server'))
     })
   }
 }
